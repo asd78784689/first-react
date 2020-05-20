0ecafbff11f430bf20baead642085f58c12cbe08
nameXJ <380407585@qq.com>
2020-05-20 13:38:48 +0800

    修改3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// 应用的根组件
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react'
+import {Button , message} from 'antd'
+
+const success = ()=>{
+	message.success('被点击了！');
 }
 
-export default App;
+export default class App extends Component {
+
+	//点击事件
+	handleClick = ()=>{
+		// alert('被点击了');
+		message.success('被点击了！');
+	}
+
+	render(){
+		return(
+			<div>
+				App
+				<Button type="primary" onClick={this.handleClick}>Button</Button>
+			</div>
+		)
+	}
+}
